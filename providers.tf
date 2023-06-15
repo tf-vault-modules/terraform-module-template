145a9f3f2c48613c9777734a3429d2a121adeb29
Myros <myros.net@gmail.com>
2023-06-15 12:52:45 -0400

    Init

--- providers.tf
+++ providers.tf
@@ -1,13 +1,5 @@
 terraform {
-  required_version = ">= 1.0.7"
+  required_version = ">= 1.5.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0, < 5.0.0"
-    }
-    awscc = {
-      source  = "hashicorp/awscc"
-      version = ">= 0.24.0"
-    }
   }
 }
